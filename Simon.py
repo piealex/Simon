43a812cad5e2ab8027e1a71c28480097ab910071
piealex <itspiealex@gmail.com>
2021-12-16 20:28:04 -0700

    Amazon Login
    amazon login

--- Simon.py
+++ Simon.py
@@ -2,20 +2,37 @@
 
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
-#ser = Service("/Users/piealex/Simon/chromedriver")
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+import time
+
 ser = Service("./chromedriver")
 op = webdriver.ChromeOptions()
 driver = webdriver.Chrome(service=ser, options=op)
 
-#chromedriver = "/Users/piealex/Simon"
-
-#driver = webdriver.Chrome(executable_path='/Users/piealex/Simon/chromedriver')
-
-#driver = webdriver.Chrome(chromedriver)
-
 #opening a webpage
 
-#driver = webdriver.Chrome()
-driver.get("https://www.amazon.com/")
+driver.get("https://www.amazon.com/ap/signin?_encoding=UTF8&openid.assoc_handle=usflex&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.mode=checkid_setup&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0&openid.ns.pape=http%3A%2F%2Fspecs.openid.net%2Fextensions%2Fpape%2F1.0&openid.pape.max_auth_age=0&openid.return_to=https%3A%2F%2Fwww.amazon.com%2Fgp%2Fyourstore%2Fhome%3Fie%3DUTF8%26action%3Dsign-out%26path%3D%252Fgp%252Fyourstore%252Fhome%26ref_%3Dnav_AccountFlyout_signout%26signIn%3D1%26useRedirectOnSuccess%3D1")
+
+#Starting Login Input
+try:
+	login = driver.find_element(By.ID, 'ap_emai')
+except:
+	print("login not found")
+	driver.quit()
+
+login.clear()
+login.send_keys("")
+login.send_keys(Keys.RETURN)
+
+password = driver.find_element(By.ID, 'ap_password')
+
+password.clear()
+password.send_keys("")
+password.send_keys(Keys.RETURN)
+
+#End of Login
+
+time.sleep(5)
 
 driver.quit()
